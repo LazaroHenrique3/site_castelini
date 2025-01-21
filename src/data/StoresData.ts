922b41bbe31839c4b84455ae816af032342dcb2d
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2025-01-20 23:55:56 -0300

    feat: adding-stores-in-storeData

--- src/data/StoresData.ts
+++ src/data/StoresData.ts
@@ -70,7 +70,7 @@ export const stores: IStoreType[] = [{
     city: "Campos de Júlio",
     uf: "MT",
     number: "4",
-    address: "AV VALDIR MASUTTI N°288S - CENTRO",
+    address: "Av Valdir Masutti N°288S - CENTRO",
     openingHours: {
         week: {
             startDay: "Seg",
@@ -86,5 +86,467 @@ export const stores: IStoreType[] = [{
         }
     },
     cellPhone: "(65) 3383-3494",
+    description: "Atendimento"
+},
+{
+    city: "Canarana",
+    uf: "MT",
+    number: "5",
+    address: "Rua Guarita N°205 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "09h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "09h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(67) 9965-44986",
+    description: "Atendimento"
+},
+{
+    city: "Colíder",
+    uf: "MT",
+    number: "6",
+    address: "Avenida Marechal Rondon N°1082 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "17h"
+        }
+    },
+    cellPhone: "(66) 3541-4567",
+    description: "Atendimento"
+},
+{
+    city: "Guarantã do Norte",
+    uf: "MT",
+    number: "7",
+    address: "Rua dos Cajueiros N°235 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "09h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "09h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(66) 9999-98633",
+    description: "Atendimento"
+},
+{
+    city: "Jaciara",
+    uf: "MT",
+    number: "8",
+    address: "Avenida Antônio Ferreira Sobrinho N°1285 - Jaciara",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "12h"
+        }
+    },
+    cellPhone: "(66) 3461-3452",
+    description: "Atendimento"
+},
+{
+    city: "Juara",
+    uf: "MT",
+    number: "9",
+    address: "Avenida Rio Arinos N°798-W - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "09h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "09h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(66) 3556-1191",
+    description: "Atendimento"
+},
+{
+    city: "Juína",
+    uf: "MT",
+    number: "10",
+    address: "Avenida Mato Grosso N°679 - Eixo Comercial I",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "09h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "09h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(67) 9989-0659",
+    description: "Atendimento"
+},
+{
+    city: "Matupá",
+    uf: "MT",
+    number: "11",
+    address: "Av. Victor Fidelis Donini N°2824 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "09h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "09h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(66) 9971-43970",
+    description: "Atendimento"
+},
+{
+    city: "Nova Mutum",
+    uf: "MT",
+    number: "12",
+    address: "Avenida Mutum N°552N - Nova Mutum",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "16h"
+        }
+    },
+    cellPhone: "(65) 3308-3110",
+    description: "Atendimento"
+},
+{
+    city: "Peixoto de Azevedo",
+    uf: "MT",
+    number: "13",
+    address: "Avenida Brasil N°111 - Centro Antigo",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "09h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "09h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(66) 9999-57535",
+    description: "Atendimento"
+},
+{
+    city: "Primavera do Leste",
+    uf: "MT",
+    number: "14",
+    address: "Avenida Pôrto Alegre N°793 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "14h"
+        }
+    },
+    cellPhone: "(66) 3498-5040",
+    description: "Atendimento"
+},
+{
+    city: "Sapezal",
+    uf: "MT",
+    number: "15",
+    address: "Av Antonio Andre Maggi, (Quadra 54 Lote 16) N°1070SW - Loteamento Cidezal",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "09h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "09h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(65) 3383-3494",
+    description: "Atendimento"
+},
+{
+    city: "Sinop",
+    uf: "MT",
+    number: "16",
+    address: "Avenida Governador Júlio Campos N°765 - Setor Comercial - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "09h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "09h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(66) 2102-7888",
+    description: "Atendimento"
+},
+{
+    city: "Sorriso",
+    uf: "MT",
+    number: "17",
+    address: "Avenida Natalino João Brescansin N°241 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "16:30h"
+        }
+    },
+    cellPhone: "(66) 3545-1054",
+    description: "Atendimento"
+},
+{
+    city: "Chapadão do Sul",
+    uf: "MS",
+    number: "18",
+    address: "Avenida Seis N°785 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "07:00h",
+            endHour: "17:30h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "07h",
+            endHour: "12h"
+        }
+    },
+    cellPhone: "(67) 3562-2488",
+    description: "Atendimento"
+},
+{
+    city: "Costa Rica",
+    uf: "MS",
+    number: "19",
+    address: "Av. José Ferreira da Costa N°1435 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "09h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "09h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(67) 9960-52171",
+    description: "Atendimento"
+},
+{
+    city: "Coxim",
+    uf: "MS",
+    number: "20",
+    address: "Av. Virgínia Ferreira N°547 - Bairro Flávio Garcia",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "12h"
+        }
+    },
+    cellPhone: "(67) 3291-3616",
+    description: "Atendimento"
+},
+{
+    city: "São Gabriel do Oeste",
+    uf: "MS",
+    number: "21",
+    address: "Rua Paraná N°1316 - São Gabriel do Oeste",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "13h"
+        }
+    },
+    cellPhone: "(67) 3295-3743",
+    description: "Atendimento"
+},
+{
+    city: "Sonora",
+    uf: "MS",
+    number: "22",
+    address: "Avenida do Povo N°725 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "12:30h"
+        }
+    },
+    cellPhone: "(67) 3254-2901",
+    description: "Atendimento"
+},
+{
+    city: "Douradina",
+    uf: "PR",
+    number: "23",
+    address: "Avenida Barão do Rio Branco N°682 - CENTRO",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08:30h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "14h"
+        }
+    },
+    cellPhone: "(44) 3663-1569",
+    description: "Atendimento"
+},
+{
+    city: "Icaraíma",
+    uf: "PR",
+    number: "24",
+    address: "Avenida Hermes Vissoto N°944 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "12h"
+        }
+    },
+    cellPhone: "(44) 3665-1429",
+    description: "Atendimento"
+},
+{
+    city: "Ivaté",
+    uf: "PR",
+    number: "25",
+    address: "Avenida Rio de Janeiro N°2423 - Centro",
+    openingHours: {
+        week: {
+            startDay: "Seg",
+            endDay: "Sex",
+            startHour: "08h",
+            endHour: "18h"
+        },
+        weekend: {
+            startDay: "Sáb",
+            endDay: "Sáb",
+            startHour: "08h",
+            endHour: "14h"
+        }
+    },
+    cellPhone: "(44) 3673-1210",
     description: "Atendimento"
 }]
