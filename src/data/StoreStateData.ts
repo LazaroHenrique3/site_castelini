a4d26ae43c88191efd7d7264614b9156026be212
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2025-01-23 00:16:19 -0300

    feat: creating-our-stores-page

---
+++ src/data/StoreStateData.ts
@@ -0,0 +1,7 @@
+import { SelectOption } from "@/components/input-select";
+
+export const storeStateData: SelectOption[] = [
+    { value: '18', label: 'Paraná' },
+    { value: '12', label: 'Mato Grosso do Sul' },
+    { value: '13', label: 'Mato Grosso' },
+];
