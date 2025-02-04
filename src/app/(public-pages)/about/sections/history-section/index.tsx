738a535f3d2d6727e94feb4ea17640b3aee81f51
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2025-02-03 23:51:06 -0300

    feat: adjusting-responsiveness-of-about-and-fix-file-send-email

--- src/app/(public-pages)/about/sections/history-section/index.tsx
+++ src/app/(public-pages)/about/sections/history-section/index.tsx
@@ -27,7 +27,7 @@ export const HistorySection = () => {
                     subtitle="Desde 1985, a Castelini Confecções cresce ao lado de seus parceiros, levando moda e inovação para diversas cidades do Brasil."
                 />
                 <div className="flex flex-col-reverse md:flex-row items-center gap-16">
-                    <div data-aos="fade-right" className="flex flex-col xl:w-[980px]">
+                    <div data-aos="fade-right" className="flex flex-col max-w-[650px] 3xl:max-w-[880px]">
                         <h3 className="text-2xl md:text-3xl font-bold text-primary-gray mb-4 uppercase">O Início de um Sonho</h3>
                         <TextBlockPrimary text="Em setembro de 1985, na cidade de Douradina-PR, dois casais da mesma família decidiram empreender juntos, nascia a Castelini Confecções. Com o passar do tempo o negócio começou a crescer, e novas unidades começaram a serem abertas, nas cidades próximas, como Ivaté-PR em 1992 e Icaraíma-PR em 1993." borderLeftOrRight="left" />
                         <TextBlockSecondary text="Esses empresários, logo compreenderam que para continuar crescendo de forma sustentável, precisariam ir mais longe, e uma nova fronteira foi então vencida, e a Castelini passava a buscar novos mercados no estado do Mato Grosso do Sul, e posteriormente no estado do Mato Grosso." />
